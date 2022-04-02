1ac4359748368cd6a422709d6e307566c6a1857b
JadroMontesB <jadro.montes99@gmail.com>
2022-04-02 14:09:09 -0700

    Class 1.3

--- config/settings.py
+++ config/settings.py
@@ -126,5 +126,5 @@ STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'home'
-# LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'hm'
+# LOGOUT_REDIRECT_URL = 'hm'
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from re import template
 from django.views.generic import TemplateView 
 
 class HomePageView(TemplateView):
-    template_name = "home.html"
+    template_name = "hm.html"
 
 
 
